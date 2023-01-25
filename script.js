816d67d72ea7c90852e118164f7b2c0ac8480558
Moonshie <moonshie.ua@gmail.com>
2023-01-25 06:01:36 +0300

    add victory counter

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ function getComputerChoice() {
     else if (choice === 2) {return "Scissors"}
 }
 
-let computerSelection = getComputerChoice();
+let computerSelection;
+let playerWinCount = 0;
+let computerWinCount = 0;
 
 function playRound() {
     let playerInput = prompt();
@@ -17,15 +19,18 @@ function playRound() {
     let firstLetter = playerInput.charAt(0);
     let playerSelection = firstLetter.toUpperCase() + playerInput.substring(1);
     console.log("Player chose " + playerSelection + ".");
+    computerSelection = getComputerChoice();
     console.log("Computer chose " + computerSelection + ".");
     if (playerSelection === computerSelection) return `It's a draw! You both chose ${computerSelection}.`
-    else if (playerSelection === "Rock" && computerSelection === "Paper") return `You Lose! Paper beats Rock.`
-    else if (playerSelection === "Rock" && computerSelection === "Scissors") return `You Win! Rock beats Scissors.`
-    else if (playerSelection === "Paper" && computerSelection === "Rock") return `You Win! Paper beats Rock.`
-    else if (playerSelection === "Paper" && computerSelection === "Scissors") return `You Lose! Scissors beat Paper.`
-    else if (playerSelection === "Scissors" && computerSelection === "Rock") return `You Lose! Rocks beats Scissors.`
-    else if (playerSelection === "Scissors" && computerSelection === "Paper") return `You Win! Scissors beat Paper.`
+    else if (playerSelection === "Rock" && computerSelection === "Paper") {computerWinCount++; return `You Lose! Paper beats Rock.`}
+    else if (playerSelection === "Rock" && computerSelection === "Scissors") {playerWinCount++; return `You Win! Rock beats Scissors.`}
+    else if (playerSelection === "Paper" && computerSelection === "Rock") {playerWinCount++; return `You Win! Paper beats Rock.`}
+    else if (playerSelection === "Paper" && computerSelection === "Scissors") {computerWinCount++; return `You Lose! Scissors beat Paper.`}
+    else if (playerSelection === "Scissors" && computerSelection === "Rock") {computerWinCount++; return `You Lose! Rocks beats Scissors.`}
+    else if (playerSelection === "Scissors" && computerSelection === "Paper") {playerWinCount++; return `You Win! Scissors beat Paper.`}
     else return "Incorrect input!";
 }
 
 console.log(playRound());
+console.log(`Player won ` + playerWinCount + ` times.`);
+console.log(`Player won ` + computerWinCount + ` times.`);
